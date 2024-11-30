33980c28365c38a80bae241acc8e13d8130afc00
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2024-11-30 23:56:56 +0530

    Time: 1 ms (86.34%) | Memory: 41 MB (28.07%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,10 +3,12 @@ class Solution {
         
         int res = 0;
         int n = x;
-        while(x != 0){
+        while(x != 0)
+        {
             int r = x %10;
             int newRes = res*10 + r;
-            if((newRes-r)/10 != res){
+            if((newRes-r)/10 != res)
+            {
                 return 0;
             }
             res = newRes;
