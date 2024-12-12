ba75d0480ef02e12733f900f0c981523f1508999
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2024-12-12 22:55:44 +0530

    Time: 14 ms (27.24%) | Memory: 56.8 MB (75.25%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,27 +1,36 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        List<Integer> numsList = new ArrayList<Integer>();
+        // List<Integer> numsList = new ArrayList<Integer>();
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                numsList.add(nums[i]);
-            }
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] > 0) {
+        //         numsList.add(nums[i]);
+        //     }
+        // }
+        
+        // for (int i = 0; i < numsList.size(); i++) {
+        //     int n = Math.abs(numsList.get(i));
+        //     if (n <= numsList.size() && numsList.get(n - 1) > 0) {
+        //         numsList.set(n - 1, -numsList.get(n - 1));
+        //     }
+        // }
+        
+        // for (int i = 0; i < numsList.size(); i++) {
+        //     if (numsList.get(i) > 0) {
+        //         return i + 1;
+        //     }
+        // }
+        
+        // return numsList.size() + 1;
+        Arrays.sort(nums);
+        int n =1;
+        for(int i : nums)
+        {
+            if(i > 0 && i == n)
+            n++;
+            else if(i > n)
+            return n;
         }
-        
-        for (int i = 0; i < numsList.size(); i++) {
-            int n = Math.abs(numsList.get(i));
-            if (n <= numsList.size() && numsList.get(n - 1) > 0) {
-                numsList.set(n - 1, -numsList.get(n - 1));
-            }
-        }
-        
-        for (int i = 0; i < numsList.size(); i++) {
-            if (numsList.get(i) > 0) {
-                return i + 1;
-            }
-        }
-        
-        return numsList.size() + 1;
-        
+        return n;
     }
 }
