a27dcdf16fe03ca904470323e3bf53d488ddaa3e
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2025-08-17 22:41:45 +0530

    Time: 3 ms (79.76%) | Memory: 45.8 MB (71.27%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -4,23 +4,22 @@ class Solution {
         if (n <= 1) return n;
 
         int[] c = new int[n];
-        Arrays.fill(c, 1); // everyone gets at least 1
-
-        // left to right
+        Arrays.fill(c, 1); 
+        
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 c[i] = c[i - 1] + 1;
             }
         }
 
-        // right to left
+        
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 c[i] = Math.max(c[i], c[i + 1] + 1);
             }
         }
 
-        // sum
+        
         int total = 0;
         for (int candies : c) total += candies;
 
