9e3fd3d717a6043ab8e232a9f46f3f982384cbfa
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2025-09-11 16:55:57 +0530

    Time: 58 ms (38.16%) | Memory: 47.7 MB (46.30%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,34 +1,32 @@
 class Solution {
+     private boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+
     public String sortVowels(String s) {
-        char [] vow = new char[s.length()];
-        String t = "";
-        int x = 0;
-        for(int i =0; i < s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            if(ch == 'A'|| ch == 'E'|| ch == 'I'|| ch == 'O'||ch == 'U'|| ch == 'a'|| ch == 'e'|| ch == 'i'|| ch == 'o'||ch == 'u')
-            {
-                t = t + '_';
-                vow[x++] = ch;
-            }
-            else 
-            t = t+ch;
-            
-            
-        }
-        String res = "";
-        Arrays.sort(vow);
-        String vows = new String(vow);
-        x = vows.lastIndexOf('\u0000') + 1;
         
-        for(char ch : t.toCharArray())
-        {
-            if(ch == '_')
-            res = res+vows.charAt(x++);
-            else
-            res=res+ch;
+         List<Character> vowels = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                vowels.add(c);
+            }
         }
-        return res;
+        
+        // Step 2: Sort vowels by ASCII
+        Collections.sort(vowels);
+        
+        // Step 3: Build result
+        StringBuilder result = new StringBuilder();
+        int vowelIndex = 0;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                result.append(vowels.get(vowelIndex++)); // replace with sorted vowel
+            } else {
+                result.append(c); // keep consonant same
+            }
+        }
+        
+        return result.toString();
         
     }
 }
