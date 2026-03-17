53d8947dbcf5c3a77d4a063b071084bd02d82546
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2026-03-17 09:28:24 +0530

    Time: 1 ms (99.91%) | Memory: 53 MB (38.64%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -13,25 +13,44 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode h1 = headA;
         ListNode h2 = headB;
-        HashSet<ListNode> sets = new HashSet<>();
-        if(h1 == null || h2 == null)
+        // HashSet<ListNode> sets = new HashSet<>();
+        // if(h1 == null || h2 == null)
+        // {
+        //     return null;
+        // }
+        // while(h1 != null)
+        // {
+        //     sets.add(h1);
+        //     h1= h1.next;
+
+        // }
+        // while(h2 != null)
+        // {
+        //     if(sets.contains(h2))
+        //     return h2;
+        //     else
+        //     h2= h2.next;
+        // }
+        // return null;
+        
+
+        //ye waala O(1) space me hai 
+        while(h1 != h2)
         {
-            return null;
-        }
-        while(h1 != null)
-        {
-            sets.add(h1);
-            h1= h1.next;
+            
+
+            if(h1 == null)
+            h1 = headB;
+            else
+            h1=h1.next;
+            if(h2 == null)
+            h2 = headA;
+            else
+            
+            h2 = h2.next;
 
         }
-        while(h2 != null)
-        {
-            if(sets.contains(h2))
-            return h2;
-            else
-            h2= h2.next;
-        }
-        return null;
+        return h1;
 
 
 
