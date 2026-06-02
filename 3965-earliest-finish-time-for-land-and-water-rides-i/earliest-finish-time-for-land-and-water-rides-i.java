d3b97e2a8e92f22a5689ffb18a13be607a52b6f0
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2026-06-03 02:03:53 +0530

    Time: 13 ms (4.30%) | Memory: 47.3 MB (10.94%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+
+        long ans = Long.MAX_VALUE;
+
+        ans = Math.min(ans,
+                solve(landStartTime, landDuration,
+                      waterStartTime, waterDuration));
+
+        ans = Math.min(ans,
+                solve(waterStartTime, waterDuration,
+                      landStartTime, landDuration));
+
+        return (int) ans;
+    }
+
+    private long solve(int[] firstStart, int[] firstDur,
+                       int[] secondStart, int[] secondDur) {
+
+        int m = secondStart.length;
+
+        int[][] rides = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            rides[i][0] = secondStart[i];
+            rides[i][1] = secondDur[i];
+        }
+
+        Arrays.sort(rides, Comparator.comparingInt(a -> a[0]));
+
+        int[] starts = new int[m];
+        long[] prefMinDur = new long[m];
+        long[] suffMinFinish = new long[m];
+
+        for (int i = 0; i < m; i++) {
+            starts[i] = rides[i][0];
+        }
+
+        prefMinDur[0] = rides[0][1];
+        for (int i = 1; i < m; i++) {
+            prefMinDur[i] = Math.min(prefMinDur[i - 1], rides[i][1]);
+        }
+
+        suffMinFinish[m - 1] = (long) rides[m - 1][0] + rides[m - 1][1];
+        for (int i = m - 2; i >= 0; i--) {
+            suffMinFinish[i] = Math.min(
+                    suffMinFinish[i + 1],
+                    (long) rides[i][0] + rides[i][1]
+            );
+        }
+
+        long res = Long.MAX_VALUE;
+
+        for (int i = 0; i < firstStart.length; i++) {
+
+            long finishFirst = (long) firstStart[i] + firstDur[i];
+
+            int pos = upperBound(starts, finishFirst);
+
+            if (pos > 0) {
+                res = Math.min(
+                        res,
+                        finishFirst + prefMinDur[pos - 1]
+                );
+            }
+
+            if (pos < m) {
+                res = Math.min(
+                        res,
+                        suffMinFinish[pos]
+                );
+            }
+        }
+
+        return res;
+    }
+
+    private int upperBound(int[] arr, long target) {
+        int l = 0, r = arr.length;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+}
