e0b2280faecad638cad91a98e39397da45bfa4d5
Mritunjay Rai <155554711+mritunjayyy07@users.noreply.github.com>
2026-06-18 00:39:53 +0530

    Time: 30 ms (42.31%) | Memory: 48 MB (42.31%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public char processStr(String s, long k) {
+        int n = s.length();
+        long L = 0;
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+                if(L > 0) L--;
+            }
+            else if (ch == '#')
+            {
+                L *=2;
+
+            }
+            else if(ch == '%') continue;
+            else { 
+                L++;
+            }
+        }
+        if (k >= L) return '.';
+        for(int i =n -1; i >= 0; i--){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+                L++;
+            }
+            else if(ch == '%'){
+                k = L -k -1;
+            }
+            else if (ch == '#'){
+                L = L/2;
+                k = (k >= L) ? k - L : k;
+
+            }
+            else {
+                L--;
+            }
+            if( k == L)
+            return ch;
+        }
+        return '.';
+        
+    }
+}
